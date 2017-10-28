c617dd31eaaa98077726d7681a4160c9bc7ee9d1
Ronaq13 <raounaqsharma@gmail.com>
2017-10-28 16:12:30 +0530

    1. Created a model for ingredient. 2. html is changed in shopping list. 3. shopping list show list of all ingredients we need to buy 4. in shopping-edit we added a form to add more ingredient.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
+// shopping-list = ingredients you need to buy
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
 
   constructor() { }
 
