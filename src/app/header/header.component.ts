752f44c1c1c18af147942d7f5218aa13d16a549c
Ronaq13 <raounaqsharma@gmail.com>
2017-11-02 13:56:55 +0530

    added navigation functanility in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() showSection = new EventEmitter<string>();
+
+  onSelect(section: string) {
+      this.showSection.emit(section);
+  }
+}
